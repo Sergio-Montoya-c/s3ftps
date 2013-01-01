accaf650dab4ca6657fa6775468222e9cf910610
Sergio Francisco Montoya Comparan <paine9999@hotmail.com>
2013-01-01 16:00:07 -0600

    First add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3ftps::Application.config.secret_token = 'efd968711ee04cae40ad24f7382f84097e2bb7ca9b7866b04bf7f7e787719809bb6218c67b698bed164fa04b4c01fe4f359eacfa79700bd0a6c0ca7e45216744'
